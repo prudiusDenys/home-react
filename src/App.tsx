95fb48ea6b1008c1f1b0c6ef3ef257bc79928bee
prudiusDenys <prdslife@gmail.com>
2020-06-25 17:07:51 +0200

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import './App.css';
 import Dialogs from "./components/Dialogs/Dialogs";
 
 export type dialogsDataType = {
-  id: number,
-  name: string,
-  text: string
+	id: number,
+	name: string,
+	text: string
 }
 
-function App() {
+function App(props: any) {
 
-  const dialogsData: Array<dialogsDataType> = [
-    {id: 1, name: 'Denis', text: 'npm start нажимал ?'},
-    {id: 2, name: 'Andrey', text: 'Да, нажал!'},
-    {id: 3, name: 'Ivan', text: 'Что такое npm ?'}
-  ]
-  return (
-    <div className="App">
-    <Dialogs dialogsData={dialogsData}/>
-    </div>
-  );
+	const dialogsData: Array<dialogsDataType> = [
+		{id: 1, name: 'Denis', text: 'npm start нажимал ?'},
+		{id: 2, name: 'Andrey', text: 'Да, нажал!'},
+		{id: 3, name: 'Ivan', text: 'Что такое npm ?'}
+	]
+	return (
+		<div>
+			<div className="App">
+				<Dialogs dialogsData={dialogsData}/>
+			</div>
+		</div>
+
+	);
 }
 
 export default App;
