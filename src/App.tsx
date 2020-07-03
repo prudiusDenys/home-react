589a3842d153d3c5198f7b04df090912ff4b76ff
prudiusDenys <prdslife@gmail.com>
2020-07-03 17:12:06 +0200

    Home task 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Dialogs from "./components/Dialogs/Dialogs";
+import Todo from "./components/Todo/Todo";
+import {v1} from "uuid";
 
 export type dialogsDataType = {
 	id: number,
 	name: string,
 	text: string
+}
+
+export type TasksType = {
+	id: string
+	item: string
+	isDone: boolean
 }
 
 function App() {
@@ -14,11 +22,45 @@ function App() {
 		{id: 1, name: 'Denis', text: 'npm start нажимал ?'},
 		{id: 2, name: 'Andrey', text: 'Да, нажал!'},
 		{id: 3, name: 'Ivan', text: 'Что такое npm ?'}
-	]
+	];
+
+	// =============================== HOME WORK NUMBER 2 ====================================
+
+	let [tasks, setTasks] = useState<Array<TasksType>>([
+		{id: v1(), item: 'React', isDone: false},
+		{id: v1(), item: 'Английский', isDone: true},
+		{id: v1(), item: 'JS', isDone: true},
+		{id: v1(), item: 'TypeScript', isDone: false},
+		{id: v1(), item: 'JestTests', isDone: false}
+	])
+
+	let [filterTask, setFilterTask] = useState('all');
+
+	let addNewTask = (newTaskValue: string) => {
+		let newTasks = [{id: v1(), item: newTaskValue, isDone: false}, ...tasks]
+		setTasks(newTasks);
+	}
+
+	let changeTasks = (valueNewFilter: string) => setFilterTask(valueNewFilter);
+
+	let deleteTask = (idValue: string) => {
+		tasks = tasks.filter(t => t.id !== idValue)
+		setTasks(tasks)
+	}
+
+	let newFilteredTasks = tasks
+	if (filterTask === 'active') newFilteredTasks = tasks.filter(t => !t.isDone);
+	if (filterTask === 'completed') newFilteredTasks = tasks.filter(t => t.isDone);
+
 	return (
 		<div>
 			<div className="App">
 				<Dialogs dialogsData={dialogsData}/>
+				<Todo title='My Todo List'
+							tasks={newFilteredTasks}
+							changeTasks={changeTasks}
+							addNewTask={addNewTask}
+							deleteTask={deleteTask}/>
 			</div>
 		</div>
 
---
+++ src/components/Todo/Todo.tsx
@@ -0,0 +1,69 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import classes from "./Todo.module.css";
+import {TasksType} from "../../App";
+
+type PropsType = {
+	title: string
+	tasks: Array<TasksType>
+	changeTasks: (value: string) => void;
+	addNewTask: (value: string) => void;
+	deleteTask: (idValue: string) => void;
+}
+
+const Todo = (props: PropsType) => {
+
+	let [value, setValue] = useState('')
+
+	let elementItem = props.tasks.map(t =>
+		<li key={t.id} className={classes.item}>
+			<input checked={t.isDone} type="checkbox"/>{t.item}
+			<button onClick={() => props.deleteTask(t.id)}>x</button>
+		</li>)
+
+
+	let showAllTasks = () => props.changeTasks('all');
+	let showActiveTasks = () => props.changeTasks('active');
+	let showCompletedTasks = () => props.changeTasks('completed');
+	let onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+	let addTaskHandler = () => {
+		props.addNewTask(value)
+		setValue('');
+	};
+
+	let onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.charCode === 13) {
+			props.addNewTask(value)
+			setValue('');
+		}
+	}
+
+	let inputRef = React.createRef<any>()
+	let showAddBlock = () => inputRef.current.classList.toggle(classes.active);
+	let hideAddBlock = () => inputRef.current.classList.remove(classes.active);
+
+
+	return (
+		<div className={classes.todo}>
+			<h2>{props.title}</h2>
+			<ul className={classes.listItem}>
+				{elementItem}
+			</ul>
+			<div className={classes.btns}>
+				<button onClick={showAllTasks}>All tasks</button>
+				<button onClick={showActiveTasks}>Active Tasks</button>
+				<button onClick={showCompletedTasks}>Completed Tasks</button>
+				<button onClick={showAddBlock}>Add Task</button>
+			</div>
+			<div className={classes.inputWrapper}>
+				<div className={classes.input} ref={inputRef}>
+					<h3>Add New Task</h3>
+					<input value={value} onChange={onChangeHandler} onKeyPress={onKeyPressHandler} type="text"/>
+					<button onClick={addTaskHandler} className={classes.TaskAdd}>+</button>
+					<h3 className={classes.close} onClick={hideAddBlock}>close</h3>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Todo;
