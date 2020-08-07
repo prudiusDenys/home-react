51d93212a1ae851e73628b6987a9a324c9b763c6
prudiusDenys <prdslife@gmail.com>
2020-08-07 14:32:10 +0200

    added Task7 (common custom select)

---
+++ src/common/Select/Select.tsx
@@ -0,0 +1,81 @@
+import React, {useState, KeyboardEvent, useEffect} from "react";
+import classes from "./Select.module.css";
+import {CountriesType} from "../../components/Task5/UsersPages/Junior";
+
+type PropsType = {
+	value: string
+	items: Array<CountriesType>
+	onChange: (selectValue: string) => void
+}
+
+export const Select = (props: PropsType) => {
+
+	const [collapsed, setCollapsed] = useState(true);
+	const [hoveredElement, setHoveredElement] = useState(props.value);
+
+	const selectedItem = props.items.find(i => i.value === props.value)
+	const hoveredItem = props.items.find(i => i.value === hoveredElement)
+
+	const onClickHandler = () => {
+		setCollapsed(!collapsed)
+	}
+	const onBlurHandler = () => {
+		setCollapsed(true)
+	}
+
+	useEffect(() => {
+		setHoveredElement(props.value)
+	}, [props.value]);
+	const onKeyDownHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+		console.log(e.keyCode)
+		if (e.keyCode === 40 || e.keyCode === 38) {
+			for (let i = 0; i < props.items.length; i++) {
+				if (props.items[i].value === hoveredElement) {
+					const pretendentElement = e.keyCode === 40 ? props.items[i + 1] : props.items[i - 1];
+					if (pretendentElement) {
+						props.onChange(pretendentElement.value);
+						return;
+					}
+				}
+			}
+			if (!selectedItem) {
+				props.onChange(props.items[0].value)
+			}
+		}
+		if (e.keyCode === 13 || e.keyCode === 27) {
+			setCollapsed(true)
+		}
+	}
+
+	const country = props.items.map(c => {
+
+		const onMouseDownHandler = () => {
+			props.onChange(c.value)
+		}
+		const onMouseEnterHandler = () => {
+			setHoveredElement(c.value)
+		}
+
+		return (
+			<div className={(hoveredItem === c) ? `${classes.country} ${classes.active}` : classes.country}
+					 key={c.value}
+					 onMouseDown={onMouseDownHandler}
+					 onMouseEnter={onMouseEnterHandler}>{c.country}
+			</div>
+		)
+	})
+	return (
+		<div className={classes.wrapper} tabIndex={0} onKeyDown={onKeyDownHandler}>
+			<h3 className={classes.active}
+					tabIndex={0}
+					onClick={onClickHandler}
+					onBlur={onBlurHandler}>{selectedItem && selectedItem.country}</h3>
+			{
+				!collapsed &&
+        <div>
+					{country}
+        </div>
+			}
+		</div>
+	)
+}
--- src/components/Task5/UsersPages/Junior.tsx
+++ src/components/Task5/UsersPages/Junior.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import {NavBar} from "../NavBar";
 import {PresCompForEditableSpan} from "../../Task6/PresCompForEditableSpan";
+import {Select} from "../../../common/Select/Select";
 
-
+export type CountriesType = {
+	value: string
+	country: string
+}
 
 export const Junior = () => {
-	return(
+
+	const countries: Array<CountriesType> = [
+		{value: '1', country: 'Belarus'},
+		{value: '2', country: 'Russia'},
+		{value: '3', country: 'Ukraine'},
+		{value: '4', country: 'Italy'},
+	]
+
+	const [value, setValue] = useState<string>(countries[0].value);
+
+	const onChange = (selectValue: string) => {
+		setValue(selectValue);
+	}
+
+	return (
 		<div>
-					<NavBar/>
-					<PresCompForEditableSpan/>
+			<NavBar/>
+			<PresCompForEditableSpan/>
+			<Select value={value} items={countries} onChange={onChange}/>
 		</div>
 	)
 }
